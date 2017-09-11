01c62036c20c3f4399d40e5ead4209658ef7a49a
Sam <31716815+SamtheOwl@users.noreply.github.com>
2017-09-11 14:48:10 -0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -12,26 +12,58 @@ public class Driver
 		//Creating stations and watchers to test
 		int numOfStations; numOfWatchers;
 		
-		Scanner input = new Scanner(System.in);
+// 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("How many broadcast station do you want to create for this test?");
-		numOfStations = input.nextInt();
+// 		System.out.println("How many broadcast station do you want to create for this test?");
+// 		numOfStations = input.nextInt();
 		
-		System.out.println("How many watchers do you want to create for this test?");
-		numOfWatchers = input.nextInt();
+// 		System.out.println("How many watchers do you want to create for this test?");
+// 		numOfWatchers = input.nextInt();
 		
-		for(int i = 0; i < numOfStations; i++)
-		{
-			BroadcastStation createStation = new BroadcastStation("Broadcast Station " + (i + 1));
-			listOfStations.add(createStation);
-		}
+// 		for(int i = 0; i < numOfStations; i++)
+// 		{
+// 			BroadcastStation createStation = new BroadcastStation("Broadcast Station " + (i + 1));
+// 			listOfStations.add(createStation);
+// 		}
 		
-		for(int i = 0; i < numOfWatchers; i++)
-		{
-			Watcher createWatcher = new Watcher("Watcher " + (i + 1));
-			listOfWatchers.add(createWatcher);
-		}
+// 		System.out.println(numOfStations + "broadcast stations created.");
 		
-		//Introduce the stations and watchers
+// 		for(int i = 0; i < numOfWatchers; i++)
+// 		{
+// 			Watcher createWatcher = new Watcher("Watcher " + (i + 1));
+// 			listOfWatchers.add(createWatcher);
+// 		}
+		
+// 		System.out.println(numOfWatchers + "watchers created.");
+		
+		numOfStations = 1;
+		numOfWatchers = 2;
+		
+		BroadcastStation stationOne = new BroadcastStation("Broadcast Station " + 1);
+		listOfStations.add(stationOne);
+		
+		Watcher watcherOne = new Watcher("Watcher " + 1);
+		listOfWatchers.add(watcherOne);
+		
+		Watcher createWatcherTwo = new Watcher("Watcher " + 2);
+		listOfWatchers.add(watcherTwo);
+		
+		System.out.println(numOfStations + "broadcast stations created.")
+		System.out.println(numOfWatchers + "watchers created.");
+		
+		watcherOne.addStation(stationOne);
+		watcherTwo.addStation(stationOne);
+		
+		stationOne.addWatcher(watcherOne);
+		stationOne.addWatcher(watcherTwo);
+		
+		stationOne.setMessage("Testing: First message.");
+		stationOne.push();
+		
+		watcherOne.getMessage();
+		watcherTwo.getMessage();
+		
+		watcherOne.showMessage():
+		watcherTwo.showMessage();
 	}
 }
